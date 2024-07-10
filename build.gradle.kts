d960d651a2c5768e17635300250d10a53f1132e9
Deftu <deftudev@gmail.com>
2024-07-10 08:07:44 +0200

    Set up automatic releasing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
+import com.modrinth.minotaur.dependencies.DependencyType
+import com.modrinth.minotaur.dependencies.ModDependency
 import dev.deftu.gradle.utils.MinecraftVersion
+import dev.deftu.gradle.utils.ModLoader
 
 plugins {
     java
@@ -18,6 +21,28 @@ toolkitLoomApi.setupTestClient()
 toolkitMultiversion.moveBuildsToRootProject.set(true)
 if (mcData.isForgeLike && mcData.version >= MinecraftVersion.VERSION_1_16_5) {
     toolkitLoomHelper.useKotlinForForge()
+}
+
+toolkitReleases {
+    modrinth {
+        projectId.set("UhitUcEo")
+        if (mcData.loader == ModLoader.FABRIC) {
+            dependencies.addAll(listOf(
+                ModDependency("P7dR8mSH", DependencyType.REQUIRED),                     // Fabric API
+                ModDependency("Ha28R6CL", DependencyType.REQUIRED),                     // Fabric Language Kotlin
+                ModDependency("mOgUt4GM", DependencyType.OPTIONAL)                      // Mod Menu
+            ))
+        } else if (mcData.version >= MinecraftVersion.VERSION_1_16_5) {
+            dependencies.addAll(listOf(
+                ModDependency("ordsPcFz", DependencyType.REQUIRED)                      // Kotlin for Forge
+            ))
+        }
+
+        if (mcData.version >= MinecraftVersion.VERSION_1_16_5) {
+            dependencies.add(ModDependency("T0Zb6DLv", DependencyType.REQUIRED))        // Textile
+            dependencies.add(ModDependency("MaDESStl", DependencyType.REQUIRED))        // Omnicore
+        }
+    }
 }
 
 dependencies {
